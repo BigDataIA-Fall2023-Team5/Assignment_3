276739def39669e2863e10407a9660108ba125b5
saniyakapur <kapur.s@northeaster.edu>
2023-11-03 15:46:38 -0400

    final

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -1,100 +1,123 @@
 import streamlit as st
+import mysql.connector
 import requests
+import os
+import pinecone
+import json
+from requests.exceptions import RequestException
 
-st.image('DocuAI.png')
 
-selected_tab = st.sidebar.selectbox("Select a tab:", ["Try", "Docs"]
 
-if selected_tab == "Try":
-    try:
-        # Code that may raise an exception
-        st.title('URL Summarization & QnA Model')
-        user_url_input = st.text_input("Extract a file", "")
-        extraction_method = st.selectbox("Choose Extraction Method", ["PyPDF", "Nougat OCR"])
+st.title("Login Page")
 
-        if extraction_method == "PyPDF":
-            response = requests.get(f"https://pdf-qna-fastapi-e398b25aae73.herokuapp.com/extract_text/?selected_tab=Try&user_url_input={user_url_input}&&extraction_method={extraction_method}")
-            st.write(response.json().get("texts"))
-            user_url_question= st.text_input("Ask a Question", "")
-            response = requests.get(f"https://pdf-qna-fastapi-e398b25aae73.herokuapp.com/get_a/?user_url_input={user_url_input}&user_question={user_url_question}")
-            st.write(response.json().get("answer"))
+# Create input widgets for username and password
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+# Create a login button
+login_button = st.button("Login")
+signup_button = st.button("Sign up")
+
+if not 'is_logged_in' in st.session_state:
+    st.session_state.is_logged_in = False
+
+ 
+# Check if the user is logged in
+if login_button:
+    data = {'username': username, 'password': password}
+    response = requests.post('http://0.0.0.0:8000/token', data=data, headers=None)
+    token_response = response.json()
+    access_token = token_response['access_token']
+    if response.status_code == 200:
+        st.session_state.is_logged_in = True
+
+if signup_button:
+    data = {'username': username, 'hashed_password': password}
+    response = requests.post('http://0.0.0.0:8000/users/register', json=data)
+    
+    if response.status_code == 200:
+        token_response = response.json()
+        user_token = token_response['username']
+        st.success(f"User {user_token} created successfully.")
+    else:
+        # If there's an error, show it in the Streamlit app
+        try:
+            response_data = response.json()  # Attempt to get JSON error message
+            error_message = response_data.get('detail', 'An error occurred.')
+        except ValueError:
+            # If response is not in JSON format, fall back to text
+            error_message = response.text or 'An error occurred.'
+        st.error(f"Failed to create user: {error_message}")
+
+ 
+if st.session_state.is_logged_in:
+        st.image('DocuAI.png')
+        selected_tab = st.sidebar.selectbox("Select a tab:", ["Try", "Docs"])
+ 
+        pinecone.init(api_key='450fe6ef-babd-4842-9a2a-6d53137a666a',environment='gcp-starter')
+        keys = ['A', 'B', 'C', 'D', 'E']
+        values = ['form1', 'form1-a', 'form1-e', 'form1-k', 'form1-n']
+        index_name = "hello"
+        index = pinecone.Index(index_name=index_name)
+        my_dict = dict(zip(values, keys))
+ 
+        # Create the dictionary
+        if selected_tab == "Try":
+            try:
+                st.title('Similarity Search using Pinecone')
+                #auth jwt
+                #index = pinecone.Index('hello')
+                selected_value = st.selectbox("Select a value:", list(my_dict.keys()))
+                # Display the selected value
+                if selected_value:
+                    selected_form = my_dict[selected_value]
+                    st.write(f"You selected: {selected_value}")
+                search_query = st.text_input('Enter your link:')
+                a = index.query(
+                    id = f"{selected_form}",
+                    filter={
+                            "link": {"$eq":f"{search_query}"},
+                            "Processing_method": {"$eq": "PyPDF"}
+                        },
+                        top_k=1,
+                        include_metadata=True
+                )
+                st.write(a['matches'][0]['metadata']['context'])
+                        
+        
+            except Exception as e:
+                # Handle the exception
+                st.write("Enter URL Above")
+        
+        elif selected_tab == "Docs":
+            st.title("DOCUMENTATION FOR NOUGHAT AND PYPDF")
+            st.subheader("NOUGHAT PROS AND CONS:")
+            st.write("""
+                    **PROS:**
+                    - Better handling of images and embedded objects within the PDF
+                    - Provides more contextual summaries for longer documents
+                    """)
+            st.write("""
+                    **CONS:**
+                    - Slower processing time as compared to pypdf
+                    - Some compatibility issues with heavily formatted documents
+                    """)
+            st.subheader("PYPDF PROS AND CONS:")
+            st.write("""
+                    **PROS:**
+                    - Faster processing speeds for text-heavy documents
+                    - Simple to use and has a straightforward integration with Streamlit
+                    """)
+            st.write("""
+                    **CONS:**
+                    - Cannot effectively handle images or embedded objects
+                    - Summaries can occasionally miss crucial information for very lengthy documents
+                    """)
+            st.subheader("ARCHITECTURE DIAGRAM")
+            st.image('pdf_qna.png')
+ 
+
+
+
 
         
-        elif extraction_method == "Nougat OCR":
-            response = requests.get(f"https://pdf-qna-fastapi-e398b25aae73.herokuapp.com/extract_text/?selected_tab=Try&user_url_input={user_url_input}&$extraction_method={extraction_method}")
-            st.write(response.json().get("texts"))
-            user_url_question= st.text_input("Ask a Question", "")
-            response = requests.get(f"https://pdf-qna-fastapi-e398b25aae73.herokuapp.com/get_a/?user_url_input={user_url_input}&user_question={user_url_question}")
-            st.write(response.json().get("answer"))
-                
-
-
-    except Exception as e:
-        # Handle the exception
-        st.write("Enter URL Above")
-
-elif selected_tab == "Docs":
-    st.title("DOCUMENTATION FOR NOUGHAT AND PYPDF")
-    st.subheader("NOUGHAT PROS AND CONS:")
-    st.write("""
-             **PROS:**
-             - Better handling of images and embedded objects within the PDF
-             - Provides more contextual summaries for longer documents
-             """)
-    st.write("""
-             **CONS:**
-             - Slower processing time as compared to pypdf
-             - Some compatibility issues with heavily formatted documents
-             """)
-    st.subheader("PYPDF PROS AND CONS:")
-    st.write("""
-             **PROS:**
-             - Faster processing speeds for text-heavy documents
-             - Simple to use and has a straightforward integration with Streamlit
-             """)
-    st.write("""
-             **CONS:**
-             - Cannot effectively handle images or embedded objects
-             - Summaries can occasionally miss crucial information for very lengthy documents
-              """)
-    st.subheader("ARCHITECTURE DIAGRAM")
-    st.image('pdf_qna.png')
-
-
-config = {
-    'user': 'bigdata',
-    'password': '#8r5BB9LBp&Nk-dN',
-    'host': 'causal-prism-404015:us-east1:bigdata',  # Use the Cloud SQL instance connection name    'database': 'bigdata',
-    'unix_socket': '/cloudsql/causal-prism-404015:us-east1:bigdata',
-}
-
-# Connect to the database
-connection = mysql.connector.connect(**config)
-
-# Function to create a connection using the config details
-@st.experimental_singleton
-def create_connection():
-    connection = None
-    try:
-        connection = mysql.connector.connect(**config)
-        if connection.is_connected():
-            db_Info = connection.get_server_info()
-            st.write(f"Connected to MySQL Server version {db_Info}")
-    except Error as e:
-        st.error(f"Error while connecting to MySQL: {e}")
-    return connection
-
-# Function to close the connection
-def close_connection(connection):
-    if connection.is_connected():
-        connection.close()
-        st.write("MySQL connection is closed")
-
-# Using the connection in Streamlit
-if 'db_connection' not in st.session_state:
-    st.session_state['db_connection'] = create_connection()
-
-
-
-
-
--- FastAPI/main.py
+++ FastAPI/main.py
@@ -13,37 +13,45 @@ import gunicorn
 import httpx
 import json
 import openai
-
+import mysql.connector
+import pymysql.cursors
 
 from datetime import datetime, timedelta
-from typing import Annotated
+from typing import Annotated, Optional
 
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+
+from google.cloud.sql.connector import Connector
+import sqlalchemy
+import pymysql
+
+# initialize Connector object
+connector = Connector()
+ 
+# function to return the database connection
+def getconn() -> pymysql.connections.Connection:
+    conn: pymysql.connections.Connection = connector.connect(
+        "causal-prism-404015:us-east1:bigdata",
+        "pymysql",
+        user="bigdata",
+        password="#8r5BB9LBp&Nk-dN",
+        db="user_auth"
+    )
+    return conn
+ 
+# create connection pool
+pool = sqlalchemy.create_engine(
+    "mysql+pymysql://",
+    creator=getconn,
+)
 
 SECRET_KEY = "787ba61010a4ed2ddd9f2108fbce1d41c54b3156821621c9310fda37d1c8bb43"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 300
 
 
-fake_users_db = {
-    "anamika": {
-        "username": "anamika",
-        "hashed_password": "$2b$12$tIuUebxpoLqU03leSs9TSu9Y4qsnMc5TSAa26hR3wQffgEFN5X4Jm",
-        "disabled": False,
-    },
-    "shruti": {
-        "username": "shruti",
-        "hashed_password": "$2b$12$dxDgdB3dLY0C9kSq6rwLdeNhv//QThEGt/xf/jOx3fsS6RxM.XS6G",
-        "disabled": False,
-    },
-    "saniya": {
-        "username": "saniya",
-        "hashed_password": "$2b$12$2Kbc9YFtLzoi4N2pMiELHOY59KBzVGwro0sNeaMwKx5gRRQYOkFXK",
-        "disabled": False,
-    }
-}
 
 def get_password_hash(password):
     return pwd_context.hash(password)
@@ -54,7 +62,7 @@ class Token(BaseModel):
     token_type: str
 
 class TokenData(BaseModel):
-    username: str | None = None
+    username: Optional[str] = None
 
 class User(BaseModel):
     username: str
@@ -65,9 +73,6 @@ class UserInDB(User):
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-#usernames = ['anamika','shruti','saniya']
-#hashed_password = ['$2b$12$tIuUebxpoLqU03leSs9TSu9Y4qsnMc5TSAa26hR3wQffgEFN5X4Jm','$2b$12$dxDgdB3dLY0C9kSq6rwLdeNhv//QThEGt/xf/jOx3fsS6RxM.XS6G','$2b$12$2Kbc9YFtLzoi4N2pMiELHOY59KBzVGwro0sNeaMwKx5gRRQYOkFXK']
 
 
 app = FastAPI()
@@ -81,14 +86,24 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+def get_user(db_connection, username: str):
+    with db_connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        cursor.execute("SELECT user, password FROM users WHERE user= %s", (username,))
+        user_record = cursor.fetchone()
 
+    if user_record:
+        # Ensure that the values are strings
+        user_in_db = UserInDB(
+            username=str(user_record['user']),
+            hashed_password=str(user_record['password'])
+        )
+        return user_in_db
+    
 
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
+def authenticate_user(username: str, password: str):
+    db_connection = getconn()
+    user = get_user(db_connection, username)
+    db_connection.close()  # Make sure to close the connection
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
@@ -96,7 +111,8 @@ def authenticate_user(fake_db, username: str, password: str):
     return user
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -107,7 +123,8 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -118,13 +135,14 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        db_connection = getconn()
+        user = get_user(db_connection, username)
+        db_connection.close()
+        if user is None:
+            raise credentials_exception
+        return user
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
 
 
 async def get_current_active_user(
@@ -135,7 +153,38 @@ async def get_current_active_user(
     return current_user
 
 
-@app.post("/token", response_model=Token)
+@app.post("/users/register", response_model=User)
+async def create_user_signup(user: UserInDB):
+    connection = getconn()
+    try:
+        cursor = connection.cursor()
+        # Check if user already exists
+        cursor.execute("SELECT * FROM users WHERE user = %s", (user.username,))
+        existing_user = cursor.fetchone()
+        if existing_user:
+            raise HTTPException(status_code=400, detail="Username already registered")
+        
+        # Hash the user's password and create new user
+        hashed_password = get_password_hash(user.hashed_password)
+        cursor.execute(
+            "INSERT INTO users (user, password, disabled) VALUES (%s, %s, %s)",
+            (user.username, hashed_password, False)
+        )
+        connection.commit()
+    except mysql.connector.Error as e:
+        connection.rollback()
+        raise HTTPException(status_code=500, detail="An error occurred while registering the user")
+    finally:
+        cursor.close()
+        connection.close()
+
+    # Return the user information excluding the hashed_password
+    return User(username=user.username)
+
+
+
+
+'''@app.post("/token", response_model=Token)
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ):
@@ -146,6 +195,25 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}'''
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    db_connection = getconn()
+    user = authenticate_user(form_data.username, form_data.password)
+    db_connection.close()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # If authentication is successful, we return a Token model.
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
@@ -167,89 +235,8 @@ async def read_own_items(
     return [{"item_id": "Foo", "owner": current_user.username}]
 
 
-with open('form_qa.json', 'r') as json_file:
-    json_data = json.load(json_file)
-# Set up OpenAI API Key
-os.environ['OPENAI_API_KEY'] = "sk-r 7R8gBmZx4MvZKqVl2wET3BlbkFJu1cLyoePHirfzn98Hv7A"  #Passkey: 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
-openai.api_key = os.getenv("OPENAI_API_KEY")
-@app.post("/get_answer/")
-def get_answer(context, user_question):
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "Use the provided text to answer the user's question. If the answer cannot be found in the text, write 'I don't know the answer'"},
-                {"role": "user", "content": f"\n\n{context}\n\nQuestion:\n{user_question}\n\nAnswer:\n"}
-            ],
-            temperature=0,
-            max_tokens=257,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        return response['choices'][0]['message']['content']
-    except Exception as e:
-        return str(e)
-
-@app.get("/get_a/")
-async def extract_text(user_url_input: str, user_question: str):
-    if user_url_input in json_data:
-        pdf_data = json_data[user_url_input]
-        pyPDF_extraction = pdf_data['pyPDF_extraction']
-        questions_and_answers = pdf_data['questions_and_answers']
-    else:
-        raise HTTPException(status_code=404, detail="PDF link not found in JSON data")
-    
-    # Get the answer using OpenAI
-    answer = get_answer(pyPDF_extraction, user_question)
-    return {
-        "pyPDF_extraction": pyPDF_extraction,
-        "answer": answer
-        }
-    
-@app.get("/extract_text/")
-async def extract_text(selected_tab: str, user_url_input: str, extraction_method: str):
-    if selected_tab != "Try":
-        raise HTTPException(status_code=400, detail="Invalid tab selected")
-    try:
-        if extraction_method == "PyPDF":
-            # Match 'user_url_input' with 'pdf_link' to get 'pyPDF_extraction' and questions/answers
-            response = requests.get(user_url_input)
-            if response.status_code == 200:
-                pdf_stream = BytesIO(response.content)
-                pdf_file = PdfReader(pdf_stream)
-                extracted_texts = []
-                for page_num in range(len(pdf_file.pages)):
-                    page = pdf_file.pages[page_num]
-                    page_text = page.extract_text()
-                    extracted_texts.append(page_text)
-                    concatenated_string = "".join(extracted_texts)
-            return {"texts": concatenated_string}
-                
-        elif extraction_method == "Nougat OCR":
-            target_directory = "https://github.com/BigDataIA-Fall2023-Team5/fastApi/"
-            wget.download(user_url_input, out=target_directory)
-            match = re.search(r'/([^/]+)$', user_url_input)
-            extracted_part = match.group(1)
-            file_path = os.path.join(target_directory, extracted_part)
-            url = "https://f3bb-35-196-208-33.ngrok-free.app/predict"
-            payload = {}
-            files = [('file', (extracted_part, open(file_path, 'rb'), 'application/pdf'))]
-            headers = {}
-            response = requests.request("POST", url, headers=headers, data=payload, files=files)
-            if response.status_code == 200:
-                extracted_text = response.text
-                if os.path.exists(file_path):
-                    os.remove(file_path)
-                
-                return {"texts": extracted_text}
-                
-        else:
-            raise HTTPException(status_code=response.status_code, detail=f"Failed to retrieve PDF from URL: {response.status_code}")
             
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Enter URL Above or check the error: " + str(e))
-
+   
 if __name__ == "__main__":
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
